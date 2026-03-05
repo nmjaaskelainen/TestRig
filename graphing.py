c02d7b8117455261129c22f867c11b91bf85764b
NMJ <nmjaaskelainen@mines.edu>
2026-03-04 23:56:11 -0700

    Initial

---
+++ graphing.py
@@ -0,0 +1,65 @@
+import serial
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+ser = serial.Serial()
+ser.port = 'COM10'
+ser.baudrate = 115200
+
+ser.open()
+
+def getFileName():
+    return datetime.now().strftime("%Y-%m-%d-%H_%M_%S.csv")
+
+onTest = False
+
+while True:
+    raw = ser.readline().decode().strip()
+    print("START")
+
+    if raw == "TESTON":
+        onTest = True
+        print("--- TEST ON ---")
+
+        Fname = getFileName()
+
+        time = []
+        voltage = []
+
+        #OnTest - speed matters
+        while onTest:
+            test = ser.readline().decode().strip()
+
+            if test == "TESTOFF":
+                onTest = False
+                print("--- TEST OFF ---")
+
+                break
+
+            data = test.split(",")
+            if len(data) == 2:
+                time.append(data[0])
+                voltage.append(data[1])
+
+        #Post test - do whatever
+        if time:
+            with open(Fname, "w") as f: 
+                f.write("Time (ms), Voltage (V)\n")
+                for t, v in zip(time, voltage):
+                    f.write(f"{t},{v}\n")
+            print("--- DATA SAVED ---")
+
+            #plot (non-blocking)
+            plt.figure(1)
+            plt.clf()
+            plt.plot([int(t) for t in time], [float(v) for v in voltage])
+            plt.title(f"Test: {Fname}")
+            plt.xlabel("Time (ms)")
+            plt.ylabel("Voltage (V)")
+            plt.pause(0.1)
+            plt.show(block=False)
+
+            print("--- READY --- \n \n")
+
+        else:
+            print("--- ERROR ---")   
